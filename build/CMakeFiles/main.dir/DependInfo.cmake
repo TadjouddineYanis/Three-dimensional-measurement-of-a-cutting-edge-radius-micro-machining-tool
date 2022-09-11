79e3dc02b69d7633d6d2d903bdbbcf4349cb94fa
Tadjouddine <tadjouddine@tadjouddine-VivoBook-ASUSLaptop>
2022-09-11 17:05:01 +0200

    contains makefile file

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/FunctionsGivesListsRadius.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/FunctionsGivesListsRadius.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/FunctionsGivesRadiusTool.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/FunctionsGivesRadiusTool.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/LineFitting.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/LineFitting.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/figures.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/figures.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/main.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/main.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/pbPlots.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/pbPlots.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/supportLib.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/supportLib.cpp.o"
+  "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/source/visualisation.cpp" "/home/tadjouddine/Three-dimensional-measurement-of-a-cutting-edge-radius-micro-machining-tool/build/CMakeFiles/main.dir/source/visualisation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../header"
+  "/usr/local/include/opencv4"
+  "/usr/include/vtk-7.1"
+  "/usr/include/freetype2"
+  "/usr/include/pcl-1.10"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/source/FunctionsGivesListsRadius.cpp.o"
+  "CMakeFiles/main.dir/source/FunctionsGivesRadiusTool.cpp.o"
+  "CMakeFiles/main.dir/source/LineFitting.cpp.o"
+  "CMakeFiles/main.dir/source/figures.cpp.o"
+  "CMakeFiles/main.dir/source/main.cpp.o"
+  "CMakeFiles/main.dir/source/pbPlots.cpp.o"
+  "CMakeFiles/main.dir/source/supportLib.cpp.o"
+  "CMakeFiles/main.dir/source/visualisation.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
